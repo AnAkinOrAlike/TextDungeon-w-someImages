ae9585e8cb6cf8f97ace8e041fed6a9ea49b2c67
AnAkinOrAlike <andresacm166@gmail.com>
2026-01-20 19:45:34 -0400

    Algunos cambios en la pagina, solo me aburri

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+let palabra = "Fuego";
+let truePal = palabra.split("");
+let guess = "W";
+function Letras() {
+
+}
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Bienvenido a mi juego del ahorcado</h1>
+      <p>Encuentra todas las letras de la palabra</p>
+      <div className='guessesBlock'>
+        <div className='blocked'>{truePal[0]}</div>
+        <div className='blocked'>{truePal[1]}</div>
+        <div className='blocked'>{truePal[2]}</div>
+        <div className='blocked'>{truePal[3]}</div>
+        <div className='blocked'>{truePal[4]}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
